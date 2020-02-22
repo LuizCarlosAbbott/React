a09ff53b05b86354c6244e8ac7ebaaf4dc3c30a6
luiz740 <luizcarlos_abbott@hotmail.com>
2020-02-21 21:32:34 -0300

    Configuração inicial do projeto

--- GraphQLCod3r/projeto/backend/index.js
+++ GraphQLCod3r/projeto/backend/index.js
@@ -1,6 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server')
-const { importSchema } = require('graphql-import')
-const resolvers = require('./resolvers')
+require('dotenv').config();
+const { ApolloServer, gql } = require('apollo-server');
+const { importSchema } = require('graphql-import');
+const resolvers = require('./resolvers');
 
 const schemaPath = './schema/index.graphql'
 const server = new ApolloServer({
--- GraphQLCod3r/projeto/backend/knexfile.js
+++ GraphQLCod3r/projeto/backend/knexfile.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
 // Update with your config settings.
 
 module.exports = {
-  client: 'mysql',
+  client: "postgresql",
   connection: {
-    database: 'projeto-final',
-    user:     'root',
-    password: 'root'
+    host: process.env.APP_DB_HOST,
+    database: process.env.APP_DB_NAME,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD
   },
   pool: {
     min: 2,
     max: 10
   },
   migrations: {
-    tableName: 'knex_migrations'
+    tableName: "knex_migrations"
   }
 };
